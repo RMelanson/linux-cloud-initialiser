b4e05aa603c7f308150810421cf911a6c3e9e8dc
Robin Melanson <remcogold@gmail.com>
2019-05-08 12:27:42 -0400

    Update cloudIntegrationBootStrap.sh

--- cloudIntegrationBootStrap.sh
+++ cloudIntegrationBootStrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 bootstrapDir=$PWD
-bootstrap=$bootstrapDir/appMicroServicesBootstrap.sh
+bootstrap=$bootstrapDir/cloudIntegrationBootStrap.sh
 
 # Ensure script is running under root
 if [ "$EUID" -ne 0 ]
