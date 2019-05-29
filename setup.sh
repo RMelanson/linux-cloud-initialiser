87aaff69ad63c1236b3a89222be16f7b9b2c5f3f
Robin Melanson <remcogold@gmail.com>
2019-05-29 19:33:42 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 setupName=setup.sh:
 # SETUP THE REQUIRED ENVIRONMENTt
-echo setup.sh: EXECUTING: ./env/setEnv.sh
+echo $setupName: EXECUTING: . ./env/setEnv.sh
 . ./env/setEnv.sh
 
 # SETUP RECOVERY
 
 # INSTALL $daemon AS A SERVICE
 echo $setupName EXECUTING: . ./installs/addAppServices.sh
-. ./installs/bootstraps/appServicesBootStrap.sh
+./installs/bootstraps/appServicesBootStrap.sh
 
 # SETUP RECOVERY
 echo $setupName EXECUTING: . ./installs/setRecovery.sh
