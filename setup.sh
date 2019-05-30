535b1e605b343d2c50e3002720eebf7c454137ec
Robin Melanson <remcogold@gmail.com>
2019-05-30 01:23:30 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ setupName=setup.sh:
 echo $setupName: EXECUTING: . ./env/setEnv.sh
 . ./env/setEnv.sh
 
-# SETUP RECOVERY
-echo $setupName EXECUTING: . ./installs/setRecovery.sh
-. ./installs/setRecovery.sh
+# SETUP RECOVERY  --- TO DO (FIX) ---
+#echo $setupName EXECUTING: . ./installs/setRecovery.sh
+#. ./installs/setRecovery.sh
 
 # INSTALL APPS_SERVICE DAEMON
 echo "$setupName EXECUTING: ./installs/appServicesBootStrap.sh"
