9a55d78d26db48f31ba58e7e8b58fabcda85368d
Robin Melanson <remcogold@gmail.com>
2019-05-30 01:28:21 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,21 +6,21 @@ echo $setupName: EXECUTING: . ./env/setEnv.sh
 . ./env/setEnv.sh
 
 # SETUP RECOVERY  --- TO DO (FIX) ---
-echo AAAAAAAAAAAAAAAAAAAAAAA
+echo BBBBBBBBBBBBBBBBBBBBBBB
 #echo $setupName EXECUTING: . ./installs/setRecovery.sh
 #. ./installs/setRecovery.sh
 
 # INSTALL APPS_SERVICE DAEMON
-echo BBBBBBBBBBBBBBBBBBBBBBB
+echo CCCCCCCCCCCCCCCCCCCCCCC
 echo "$setupName EXECUTING: ./installs/appServicesBootStrap.sh"
 ./installs/bootstraps/appServicesBootStrap.sh
 
 x# ADD CLOUD_SERVICE SERVICE
-echo CCCCCCCCCCCCCCCCCCCCCCC
+echo DDDDDDDDDDDDDDDDDDDDDDD
 echo $setupName EXECUTING: . ./installs/addCloudInitialiserService.sh
 . ./installs/addCloudInitialiserService.sh
 
 # START CLOUD_SERVICE SERVICE
-echo DDDDDDDDDDDDDDDDDDDDDDD
+echo EEEEEEEEEEEEEEEEEEEEEEE
 echo $setupName EXECUTING: . ./installs/startCloudInitialiserService.sh
 . ./installs/startCloudInitialiserService.sh
