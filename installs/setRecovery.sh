5d70493832f65ad788176a80006e476ecd9d054d
Robin Melanson <remcogold@gmail.com>
2019-05-30 16:23:25 -0400

    Update setRecovery.sh

--- installs/setRecovery.sh
+++ installs/setRecovery.sh
@@ -1,20 +1,3 @@
-#! /bin/bash
-clear
-
-# SETUP CONFIGURATION ENVIRONMENT AND PARAMETERS
-
-echo reinstall.sh:  EXECUTING ENVIRONMENT SETUP: . ./env/setEnv.sh
-. ./env/setEnv.sh
-
-echo reinstall.sh:  EXECUTING cp $bootstrap $parentDir
-cp $bootstrap $parentDir
-cd $parentDir
-
-echo reinstall.sh:  EXECUTING REMOVE OLD INSTALLATION: rm -rf $installDir
-rm -rf $installDir
-
-echo reinstall.sh:  EXECUTING BOOTSTRAP: . ./$bootstrap
-. ./$bootstrap
-
-echo reinstall.sh:  EXECUTING BOOTSTRAP: rm $bootstrap
-rm $bootstrap
+SET UP REINSTALL
+echo Setup.sh:  EXECUTING: cp $installDir/reinstall.sh $parentDir/$pkg-RECOVERY.sh
+cp $installDir/reinstall.sh $parentDir/$pkg-RECOVERY.sh
