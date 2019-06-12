61a5e4bcb8e008983369b60be50c9d4eca659e8d
Robin Melanson <remcogold@gmail.com>
2019-06-11 21:26:21 -0400

    Update startCloudInitialiserService.sh

--- installs/startCloudInitialiserService.sh
+++ installs/startCloudInitialiserService.sh
@@ -1,2 +1,2 @@
-echo "EXECUTING startCloudInitialiserService.sh: service apps start java $serverPort -jar /opt/CI/services/jobs/apps/jars/cloud-initialiser-1.3.5.RELEASE.war"
-service apps start java $serverPort -jar /opt/CI/services/jobs/apps/jars/cloud-initialiser-1.3.5.RELEASE.war
+echo "EXECUTING startCloudInitialiserService.sh: apps start java $serverPort -jar /opt/CI/services/jobs/apps/jars/cloud-initialiser-1.3.5.RELEASE.war"
+apps start java $serverPort -jar /opt/CI/services/jobs/apps/jars/cloud-initialiser-1.3.5.RELEASE.war
