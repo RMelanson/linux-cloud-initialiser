7b2e14dd0d148696eae40753a358157725761b08
Robin Melanson <remcogold@gmail.com>
2019-07-05 00:54:48 -0400

    Create updateModules.sh

---
+++ installs/updateModules.sh
@@ -0,0 +1,17 @@
+echo "$setupName: EXECUTING: . ./env/setEnv.sh $*"
+. ./env/setEnv.sh $*
+
+#ToDo 
+# For Each Module in Directory $modulesDir
+# {
+#   Do Git Fetch
+#   Do Git Pull
+#   Do Git Commit -m "MODULES UPDATE"
+#   Do Git Push
+# }
+# For Linux Cloud Initialiser
+# {
+#   Do Git Add *
+#   Do Git Commit -m "MODULES UPDATE"
+#   Do Git Push
+# }  
