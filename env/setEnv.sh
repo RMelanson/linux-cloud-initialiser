8ca55bfe5ac0d620fd22514610cdbb943af22beb
Robin Melanson <remcogold@gmail.com>
2019-07-06 18:30:24 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -5,6 +5,7 @@ bootstrap="cloudInitialiserBootStrap.sh"
 gitRepo="linux-scripts-cloud-initialiser"
 
 #SET UP INSTALLATION DIRECTORY
+daemon=daemon=/cloudInit.sh
 pkg=CLOUD_INITIALISER
 scriptType="apps"
 progType="jars"
