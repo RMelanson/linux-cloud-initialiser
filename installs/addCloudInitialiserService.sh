5f66dc60c022a8fd6c895bf89d3cbf5090d0fad9
Robin Melanson <remcogold@gmail.com>
2019-07-06 03:13:28 -0400

    Update addCloudInitialiserService.sh

--- installs/addCloudInitialiserService.sh
+++ installs/addCloudInitialiserService.sh
@@ -5,7 +5,7 @@ echo $setupName: EXECUTING ./env/setEnv.sh
 
 # COPY CLOUD INTEGRATION APPLICATION TO SERVICE APPLICATION DIRECTORY
 echo addCloudInitialiserService.sh EXECUTING: cp -rf $installDir/services $appServicesDir
-cp -rf $installDir/services $appServicesDir
+cp -rf $installDir/services/* $appServicesDir
 
 # COPY THE JAR LIBRARIES TO THE CLOUD INITIALISER DIRECTORY
 $cp -rf $installDir/subModules/libs/* $appServicesDir -y
