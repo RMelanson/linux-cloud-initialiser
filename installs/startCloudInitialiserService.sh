7f123be012549c34fa45f03eeaabe304126dec95
Robin Melanson <remcogold@gmail.com>
2019-07-06 18:23:17 -0400

    Update startCloudInitialiserService.sh

--- installs/startCloudInitialiserService.sh
+++ installs/startCloudInitialiserService.sh
@@ -1,2 +1,2 @@
-echo "EXECUTING startCloudInitialiserService.sh: apps start java $serverPort -jar $appServicesDir/jobs/java/libs/cloud-initialiser-1.3.5.RELEASE.war"
-apps start java $serverPort -jar $appServicesDir/jobs/java/libs/cloud-initialiser-1.3.5.RELEASE.war
+echo "EXECUTING startCloudInitialiserService.sh: $daemon start java $serverPort -jar $appServicesDir/jobs/java/libs/cloud-initialiser-1.3.5.RELEASE.war"
+$daemon start java $serverPort -jar $appServicesDir/jobs/java/libs/cloud-initialiser-1.3.5.RELEASE.war
